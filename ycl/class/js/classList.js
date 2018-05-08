07e172b5366e1cef6ccdaa1489782e9f33d1ee0e
YanChunlin96 <1519780292@qq.com>
2018-05-08 15:28:19 +0800

    ycl-数据表格

---
+++ ycl/class/js/classList.js
@@ -0,0 +1,25 @@
+var myclass=[{
+    "id" : "c_001",
+    "c_name" : "喵喵班",
+    "c_num" : "30",
+    "c_teacher" : "张三",
+    "c_address" : "一教001"
+},{
+    "id" : "c_002",
+    "c_name" : "丫丫班",
+    "c_num" : "35",
+    "c_teacher" : "李四",
+    "c_address" : "一教002"
+},{
+    "id" : "c_003",
+    "c_name" : "臭臭班",
+    "c_num" : "25",
+    "c_teacher" : "王二",
+    "c_address" : "一教003"
+},{
+    "id" : "c_004",
+    "c_name" : "朵朵班",
+    "c_num" : "25",
+    "c_teacher" : "王二",
+    "c_address" : "一教003"
+}];
