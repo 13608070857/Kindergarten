f8593d193a5eefc59d0a3e9a4dff6f02ccdec1e5
17711011036 <37230970+17711011036@users.noreply.github.com>
2018-05-09 16:29:28 +0800

    lin

--- ljh/staff/js/classList.js
+++ ljh/staff/js/classList.js
@@ -1,71 +1,113 @@
 var myclass=[{
-    "id" : "c_001",
-    "c_name" : "喵喵班",
-    "c_num" : "30",
-    "c_teacher" : "张三",
+    "id" : "y_001",
+    "c_name" : "林家辉",
+    "c_num" : "教师",
+    "c_teacher" : "教育部",
     "c_teacher2" : "李老三",
-    "c_address" : "大班"
+    "c_address" : "18808080888"
 },{
-    "id" : "c_002",
-    "c_name" : "丫丫班",
-    "c_num" : "35",
-    "c_teacher" : "李四",
+    "id" : "y_002",
+    "c_name" : "林久",
+    "c_num" : "教师",
+    "c_teacher" : "教育部",
     "c_teacher2" : "秀老四",
-    "c_address" : "中班"
+    "c_address" : "17711011011"
 },{
-    "id" : "c_003",
-    "c_name" : "臭臭班",
-    "c_num" : "25",
-    "c_teacher" : "王二",
+    "id" : "y_003",
+    "c_name" : "王玖拾",
+    "c_num" : "教师",
+    "c_teacher" : "教育部",
     "c_teacher2" : "麻小二",
-    "c_address" : "小班"
+    "c_address" : "15817293811"
 },{
-    "id" : "c_004",
-    "c_name" : "朵朵班",
-    "c_num" : "25",
-    "c_teacher" : "王二",
+    "id" : "y_004",
+    "c_name" : "卢伟霞",
+    "c_num" : "保育员",
+    "c_teacher" : "生活部",
     "c_teacher2" : "麻小二",
-    "c_address" : "大班"
+    "c_address" : "17363628173"
 },{
-    "id" : "c_005",
-    "c_name" : "朵朵班",
-    "c_num" : "25",
-    "c_teacher" : "王二",
+    "id" : "y_005",
+    "c_name" : "王玉婷",
+    "c_num" : "保育员",
+    "c_teacher" : "生活部",
     "c_teacher2" : "麻小二",
-    "c_address" : "中班"
+    "c_address" : "18394739214"
 },{
-    "id" : "c_006",
-    "c_name" : "朵朵班",
-    "c_num" : "25",
-    "c_teacher" : "王二",
+    "id" : "y_006",
+    "c_name" : "张小米",
+    "c_num" : "保育员",
+    "c_teacher" : "生活部",
     "c_teacher2" : "麻小二",
-    "c_address" : "小班"
+    "c_address" : "1837346392"
 },{
-    "id" : "c_007",
-    "c_name" : "朵朵班",
-    "c_num" : "25",
-    "c_teacher" : "王二",
+    "id" : "y_007",
+    "c_name" : "张小米",
+    "c_num" : "保育员",
+    "c_teacher" : "生活部",
     "c_teacher2" : "麻小二",
-    "c_address" : "小班"
+    "c_address" : "1837346392"
 },{
-    "id" : "c_008",
-    "c_name" : "朵朵班",
-    "c_num" : "25",
-    "c_teacher" : "王二",
+    "id" : "y_008",
+    "c_name" : "何微微",
+    "c_num" : "保育员",
+    "c_teacher" : "安保部",
     "c_teacher2" : "麻小二",
-    "c_address" : "小班"
+    "c_address" : "16384929178"
 },{
-    "id" : "c_009",
-    "c_name" : "朵朵班",
-    "c_num" : "25",
-    "c_teacher" : "王二",
+    "id" : "y_009",
+    "c_name" : "何义东",
+    "c_num" : "门卫",
+    "c_teacher" : "安保部",
     "c_teacher2" : "麻小二",
-    "c_address" : "小班"
+    "c_address" : "19484048223"
 },{
-    "id" : "c_010",
-    "c_name" : "朵朵班",
-    "c_num" : "25",
-    "c_teacher" : "王二",
+    "id" : "y_010",
+    "c_name" : "龚和喜",
+    "c_num" : "门卫",
+    "c_teacher" : "安保部",
     "c_teacher2" : "麻小二",
-    "c_address" : "小班"
-}];
+    "c_address" : "85039282"
+},{
+    "id" : "y_011",
+    "c_name" : "单开武",
+    "c_num" : "巡逻",
+    "c_teacher" : "安保部",
+    "c_teacher2" : "麻小二",
+    "c_address" : "1563726281"
+},{
+    "id" : "y_012",
+    "c_name" : "何义东",
+    "c_num" : "巡逻",
+    "c_teacher" : "安保部",
+    "c_teacher2" : "麻小二",
+    "c_address" : "19484048223"
+},{
+    "id" : "y_013",
+    "c_name" : "杜文杰",
+    "c_num" : "监控",
+    "c_teacher" : "安保部",
+    "c_teacher2" : "麻小二",
+    "c_address" : "19484048223"
+},{
+    "id" : "y_014",
+    "c_name" : "何义东",
+    "c_num" : "保洁",
+    "c_teacher" : "保洁部",
+    "c_teacher2" : "麻小二",
+    "c_address" : "19484048223"
+},{
+    "id" : "y_015",
+    "c_name" : "谢高丽",
+    "c_num" : "保洁",
+    "c_teacher" : "保洁部",
+    "c_teacher2" : "麻小二",
+    "c_address" : "19484048223"
+},{
+    "id" : "y_016",
+    "c_name" : "雯丽丽",
+    "c_num" : "保洁",
+    "c_teacher" : "保洁部",
+    "c_teacher2" : "麻小二",
+    "c_address" : "19484048223"
+},];
