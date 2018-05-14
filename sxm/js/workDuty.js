dc5d998ca45b09de6eac9e89b43501e2a207a418
jysson <jysson1224@users.noreply.github.com>
2018-05-14 10:19:13 +0800

    sxm

--- sxm/js/workDuty.js
+++ sxm/js/workDuty.js
@@ -1,13 +1,13 @@
 var duties=[{
     "id" : "w_001",
     "w_name" : "园长办公室",
-    "w_describe" : "园长办公室",
+    "w_describe" : "管理院内大小事务",
     "w_num" : "1",
     "w_admin" : "林家辉"
 },{
     "id" : "w_002",
     "w_name" : "教学部",
-    "w_describe" : "老师的办公室",
+    "w_describe" : "老师课间休息室",
     "w_num" : "20",
     "w_admin" : "李雨儿"
 }];
